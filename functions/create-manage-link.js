c7a9dba6e221881019e877b3b2ad0b9965e554b7
Tomasz W <twieczor@hotmail.com>
2020-09-16 15:26:46 -0400

    manage subscriptions + handle stripe changes

---
+++ functions/create-manage-link.js
@@ -0,0 +1,28 @@
+exports.handler = async (_event, context) => {
+    const { user } = context.clientContext;
+  
+    const result = await faunaFetch({
+      query: `
+        query ($netlifyID: ID!) {
+          getUserByNetlifyID(netlifyID: $netlifyID) {
+            stripeID
+          }
+        }
+      `,
+      variables: {
+        netlifyID: user.sub,
+      },
+    });
+  
+    const { stripeID } = result.data.getUserByNetlifyID;
+  
+    const link = await stripe.billingPortal.sessions.create({
+      customer: stripeID,
+      return_url: process.env.URL,
+    });
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify(link.url),
+    };
+  };
